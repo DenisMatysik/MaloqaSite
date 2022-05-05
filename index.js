6534fda07dfd57f5d8b877964219570cdd5c9b0e
denis <denismatysik@gmail.com>
2022-05-05 14:42:03 +0300

    fix

--- index.js
+++ index.js
@@ -1,37 +1,58 @@
 const mainLinks = {
-    discord: "https://discord.gg/nD5AGcnx",
-    steam: "https://www.twitch.tv/ma1ojqa",
-    vk: "https://vk.com/ajiecb",
-    twitch: "https://www.twitch.tv/ma1ojqa",
-    minecraft: "https://www.twitch.tv/ma1ojqa",
+    discord:{
+        link:"https://discord.gg/nD5AGcnx",
+        imageLink: "./img/discord.png"
+    },
+    steam:{
+        link:"https://www.twitch.tv/ma1ojqa",
+        imageLink: "./img/steam.png"
+    },
+    vk:{
+        link:"https://vk.com/ajiecb",
+        imageLink: "./img/vk.png"
+    },
+    twitch:{
+        link:"https://www.twitch.tv/ma1ojqa",
+        imageLink: "./img/twitch.png"
+    },
+    minecraft:{
+        link:"https://www.twitch.tv/ma1ojqa",
+        imageLink: "./img/minecraft.png"
+    },
 }
 
-
-const btns = document.querySelectorAll(".btn");
+// const btns = document.querySelectorAll(".btn");
 const container = document.querySelector(".container");
 const nik = document.querySelector(".nik");
-nik.addEventListener("mouseenter", defaultFon)
+const nav = document.querySelector(".nav");
+
+
+window.onload = randomBackGroundImage;
+
+function randomBackGroundImage(){
+    let randomNumber = Math.ceil((Math.random()*8)); // меняется картинка 
+    container.style.backgroundImage = `url(./img/fon/${randomNumber}.jpg)`;
+}
+
+function changeFon(){
+    setInterval(randomBackGroundImage, 30000) // каждые 30секунд меняется фон
+    }   
+changeFon();
+
+
+
+
+
+
+
 
 
 for (const item of btns) {   
     item.addEventListener("click", goToSite)
-    item.addEventListener("mouseenter", changeFon)
+    // item.addEventListener("mouseenter", changeFon)
     const atr =  item.firstChild.getAttribute("name");
 
     function goToSite(){
         window.location.href = mainLinks[atr];
     }
-
-    function changeFon(){
-        setTimeout(()=>{
-            container.style.backgroundImage = `url(/img/fon/${atr}-fon.jpg)`;
-        }, 0)
-    }   
-}
-
-function defaultFon(){
-    setTimeout(()=>{
-        console.log("def")
-        container.style.backgroundImage = 'url(/img/fon/default-fon.jpg)';
-    }, 0)
 }
